e465778230cb883aa7b07b25e539a92d7367dd2b
Gabriel-Paes <paessouzagabriel@gmail.com>
2023-07-22 22:52:56 -0300

    add option and survey models

---
+++ Models/Option.cs
@@ -0,0 +1,8 @@
+﻿namespace api_survey.Models
+{
+    public class Option
+    {
+        public String Name { get; set; }
+        public int Votes { get; set; }
+    }
+}
---
+++ Models/Survey.cs
@@ -0,0 +1,9 @@
+﻿namespace api_survey.Models
+{
+    public class Survey
+    {
+        public String Title { get; set; }
+        public String Question { get; set; }
+        public ICollection<Option> Options { get; set; }
+    }
+}
